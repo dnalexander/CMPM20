f4215a932ee3bd0e0a50a20eb59d05ba83edea4c
Donovan <dnab0012@gmail.com>
2015-02-06 01:41:41 -0800

    Added movement

--- Snake/main.js
+++ Snake/main.js
@@ -12,6 +12,8 @@ sHead.height = tileSize;
 sHead.width = tileSize;
 sHead.x = 400;
 sHead.y = 500;
+sHead.prevX = sHead.x;
+sHead.prevY = sHead.y;
 sHead.direction = "right";
 sHead.image = Textures.load("http://people.ucsc.edu/~tmcqueen/Sprites/sHead.png");
 function initSnake() {
@@ -23,6 +25,8 @@ function initSnake() {
    	snake.width = tileSize;
    	snake.x = (sList.getAt(i-1).x-18);
    	snake.y = (sList.getAt(i-1).y);
+   	snake.prevX = snake.x;
+   	snake.prevY = snake.y;	
    	snake.image = Textures.load("http://people.ucsc.edu/~tmcqueen/Sprites/sBody.png");
    	sList.push(snake);
 	}
@@ -32,24 +36,70 @@ function initSnake() {
 }
 
 function updateSnake(){
+	if(gInput.left && !gInput.right && !gInput.up && !gInput.down && (sHead.direction != "right")){
+		sHead.direction = "left";
+	}
+	if(!gInput.left && gInput.right && !gInput.up && !gInput.down && (sHead.direction != "left")){
+		sHead.direction = "right";
+	}
+	if(!gInput.left && !gInput.right && gInput.up && !gInput.down && (sHead.direction != "down")){
+		sHead.direction = "up";
+	}
+	if(!gInput.left && !gInput.right && !gInput.up && gInput.down && (sHead.direction != "up")){
+		sHead.direction = "down";
+	}
 	switch(sHead.direction){
 		case "left":
+			sHead.prevX -= tileSize;
 			sHead.x -= tileSize;
 			break;
 		case "right":
+			sHead.prevX += tileSize;
 			sHead.x += tileSize;
 			break;
 		case "up":
+			sHead.prevY -= tileSize;
 			sHead.y -= tileSize;
 			break;
 		case "down":
-			sHead.y += tileSize;
+			sHead.prevY += tileSize;
+			sHead.y += tileSize;			
 			break;			
 	}
-	for(var i=0; i<sList.length; i++){
-		
+	/*console.log("left = "+gInput.left);
+	console.log("right = "+gInput.right);
+	console.log("up = "+gInput.up);
+	console.log("down = "+gInput.down);*/
+	for(var i=1; i<sList.length; i++){
+		switch(sHead.direction){
+		case "left":
+			//sList.getAt(i).x = (sList.getAt(i-1).x+18);
+			//sList.getAt(i).y = (sList.getAt(i-1).y)
+			sList.getAt(i).x = (sList.getAt(i-1).prevX+18);
+			sList.getAt(i).y = (sList.getAt(i-1).prevY);
+			break;
+		case "right":
+			//sList.getAt(i).x = (sList.getAt(i-1).x-18);
+			//sList.getAt(i).y = (sList.getAt(i-1).y)
+			sList.getAt(i).x = (sList.getAt(i-1).prevX-18);
+			sList.getAt(i).y = (sList.getAt(i-1).prevY);
+			break;
+		case "up":
+			//sList.getAt(i).x = (sList.getAt(i-1).x);
+			//sList.getAt(i).y = (sList.getAt(i-1).y+18)
+			sList.getAt(i).x = (sList.getAt(i-1).prevX);
+			sList.getAt(i).y = (sList.getAt(i-1).prevY+18);
+			break;
+		case "down":
+			//sList.getAt(i).x = (sList.getAt(i-1).x);
+			//sList.getAt(i).y = (sList.getAt(i-1).y-18)
+			sList.getAt(i).x = (sList.getAt(i-1).prevX);
+			sList.getAt(i).y = (sList.getAt(i-1).prevY-18);
+			break;			
+		}
 	}
 }
 
 initSnake();
-updateSnake();
+setInterval(updateSnake, 160);
+//setInterval(updateSnake, 1000);
